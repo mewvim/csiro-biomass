7915b61fb8e8826cc96237d832a95f783dcf84d6
mewvim <frank.paiva@toyotaconnected.com>
2025-11-08 17:01:05 -0600

    logging to get more info

--- csiro/dataset.py
+++ csiro/dataset.py
@@ -17,15 +17,21 @@ class CsiroDataset:
     )
 
     def __init__(self, username: str | None = None, key: str | None = None):
+        logger.info("Attempting to download the csiro dataset")
+
         self.username = username
         self.key = key
         self.build_kaggle_credentials()
 
+        logger.info("Configured kaggle credentials")
+
     def download_dataset(self, output: Path) -> None:
         kaggle.api.authenticate()
+        logger.info("Kaggle authentication complete")
         kaggle.api.competition_download_files(
             CsiroDataset.competition_name, path=output
         )
+        logger.info("dataset download complete")
 
     def build_kaggle_credentials(self) -> None:
         if not CsiroDataset.kaggle_config.exists():
